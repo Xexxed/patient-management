c9e177900c81654012f0619d1632339d6508fb3e
Xexxxed <bukhariahmed76@gmail.com>
2025-05-08 16:26:02 -0230

    ✨ (billing-service): Add initial implementation of billing service with gRPC support, including Dockerfile and Maven configuration

--- billing-service/src/main/java/com/pm/billingservice/grpc/BillingGRPCService.java
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGRPCService.java
@@ -1,4 +1,31 @@
 package com.pm.billingservice.grpc;
 
-public class BillingGRPCService {
+
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@GrpcService
+public class BillingGRPCService extends BillingServiceImplBase {
+
+
+    private static final Logger log = LoggerFactory.getLogger(BillingGRPCService.class);
+
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest, StreamObserver<BillingResponse> responseObserver){
+        log.info("CreateBillingAccount request received{}", billingRequest.toString());
+
+
+        BillingResponse billingResponse = BillingResponse.newBuilder().setAccountId("123456789").setStatus("SUCCESS").build();
+        responseObserver.onNext(billingResponse);
+        responseObserver.onCompleted();
+    }
+
+
+
+
 }

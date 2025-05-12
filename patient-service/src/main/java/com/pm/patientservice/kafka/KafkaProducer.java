c6ae901c9ac037f1dba9cbd6e143718d27ff8f60
Xexxxed <bukhariahmed76@gmail.com>
2025-05-12 14:02:54 -0230

    ✨ (patient-service): Add KafkaProducer class and spring-kafka dependency for message production

--- patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -1,4 +1,19 @@
 package com.pm.patientservice.kafka;
 
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class KafkaProducer {
+
+
+    private final KafkaTemplate<String,byte[]> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String topic, byte[] message) {
+        kafkaTemplate.send(topic, message);
+    }
 }

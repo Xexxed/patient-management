48059d6b96704452dab6d3a627e14a8605c630e6
Xexxxed <bukhariahmed76@gmail.com>
2025-05-15 21:11:24 -0230

    ✨ (api-gateway): Add initial API gateway setup with Spring Boot, including application properties and routing configuration

---
+++ patient-auth-service/src/main/java/com/pm/patientauthservice/PatientAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pm.patientauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientAuthServiceApplication.class, args);
+    }
+
+}

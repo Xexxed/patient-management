bbae98675e120529ae1520d45b1ea1daabc37387
Xexxxed <bukhariahmed76@gmail.com>
2025-05-16 07:57:37 -0230

    ✨ (integration-tests): Enhance AuthIntegrationTest and PatientIntegrationTest with additional test cases for login and token validation

--- integration-tests/src/test/java/AuthIntegrationTest.java
+++ integration-tests/src/test/java/AuthIntegrationTest.java
@@ -39,4 +39,79 @@ public class AuthIntegrationTest {
         System.out.println(" Generated Token: "+token);
     }
 
+
+
+    @Test
+    public void shouldReturnUnauthorizedOnInvalidLogin(){
+        //AAA(Arrange, Act, Assert)
+        //Arrange
+        String loginPayload= """
+                {
+                    "email":"invalid_testuser@test.com",
+                    "password":"wrongpassword123"
+                 }
+                """;
+        //Act
+        RestAssured
+                .given()
+                .contentType("application/json")
+                .body(loginPayload)
+                .when()
+                .post("/auth/login")
+                .then()
+                //Assert
+                .statusCode(401);
+
+
+    }
+    @Test
+    public void shouldReturnUnauthorizedOnInvalidToken(){
+        //AAA(Arrange, Act, Assert)
+        //Arrange
+        String invalidToken="invalid_token";
+        //Act
+        RestAssured
+                .given()
+                .header("Authorization", "Bearer " + invalidToken)
+                .when()
+                .get("/auth/validate")
+                .then()
+                //Assert
+                .statusCode(401);
+    }
+
+    @Test
+
+    public void shouldReturnOKOnValidToken() {
+        //AAA(Arrange, Act, Assert)
+        //Arrange
+        String loginPayload = """
+                {
+                    "email":"testuser@test.com",
+                    "password":"password123"
+                 }
+                """;
+        //Act
+        Response response = RestAssured
+                .given()
+                .contentType("application/json")
+                .body(loginPayload)
+                .when()
+                .post("/auth/login")
+                .then()
+                //Assert
+                .statusCode(200)
+                .body("token", notNullValue())
+                .extract().response();
+        String token = response.jsonPath().getString("token");
+        RestAssured
+                .given()
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .get("/auth/validate")
+                .then()
+                //Assert
+                .statusCode(200);
+    };
+
 }
--- integration-tests/src/test/java/PatientIntegrationTest.java
+++ integration-tests/src/test/java/PatientIntegrationTest.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.notNullValue;
 
 public class PatientIntegrationTest {
+
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "http://localhost:4004";
+    }
+
+    // @Test
+    @Test
+    public void shouldReturnPatientsWithValidToken() {
+        //AAA(Arrange, Act, Assert)
+        //Arrange
+        String loginPayload = """
+                {
+                    "email":"testuser@test.com",
+                    "password":"password123"
+                 }
+                """;
+        //Act
+        String token = RestAssured
+                .given()
+                .contentType("application/json")
+                .body(loginPayload)
+                .when()
+                .post("/auth/login")
+                //Assert
+                .then()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .get("token");
+        System.out.println("Generated Token: " + token);
+        //Act
+        RestAssured
+                .given()
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .get("/api/patients")
+                .then()
+                //Assert
+                .statusCode(200)
+                .body("patients", notNullValue())
+                .log().all();
+    }
 }

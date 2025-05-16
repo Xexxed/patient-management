d65d06ad1f97a26bee68792c7925c22c8fda340f
Xexxxed <bukhariahmed76@gmail.com>
2025-05-16 14:04:59 -0230

    fix: Update API endpoint in create-patient request and add delete-stack command in localstack-deploy script

--- infra-structure/localstack-deploy.sh
+++ infra-structure/localstack-deploy.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+aws --endpoint-url=http://localhost:4566 cloudformation delete-stack \
+    --stack-name patient-management
 aws --endpoint-url=http://localhost:4566 cloudformation deploy \
     --stack-name patient-management \
     --template-file "./cdk.out/localstack.template.json"
